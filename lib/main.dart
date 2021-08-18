d0d84387b794a7af2e7f99d90461c0bc81e35583
HiskelKelemework <51155500+HiskelKelemework@users.noreply.github.com>
2021-08-18 13:44:55 +0300

    removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:myapp/blocs/test_bloc.dart';
-import 'package:myapp/blocs/test_event.dart';
-import 'package:myapp/blocs/test_state.dart';
-import 'package:myapp/star_stateful.dart';
 
 void main() => runApp(MyApp());
 
